c8a7d4885715195554ead6e973dcd4686c74b3c8
SergioTech <sergegamligo1@gmail.com>
2026-05-27 05:04:52 +0000

    Change Chrome executable path for Puppeteer

--- PhareProject/whatsapp/bot.js
+++ PhareProject/whatsapp/bot.js
@@ -5,7 +5,7 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
-     executablePath: '/opt/render/.cache/puppeteer/chrome/linux-149.0.7827.22/chrome-linux64/chrome',
+     executablePath: '/usr/bin/chromium-browser',
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
 });
